ce0a1d9957945e061a9dacd039e7477be242823d
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-02-24 12:16:37 +0900

    first commit

---
+++ src/chap01/RunStatementExample.java
@@ -0,0 +1,14 @@
+package chap01;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1; // 변수 x를 선언하고 1을 저장
+		int y = 2; // 변수 y를 선언하고 2를 저장
+		int result = x + y; // 변수 result를 선언하고 x와 y를 더한 값을 저장
+		System.out.println(x + "+" + y + "=" + result); // 콘솔에 출력하는 메소드 호출
+		
+		// 실행 단축기 :ctrl + f11
+		// 명령문은 세미콜론으로 종료되야 한다.
+		// 명령문들이 모여서 프로그램을 만들고 실행된다.
+	}
+}
