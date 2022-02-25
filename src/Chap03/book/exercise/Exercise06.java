7276acf84d9ab40c33b00c786c7585addcc70a17
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-02-25 19:27:52 +0900

    20220225 book exercise

---
+++ src/Chap03/book/exercise/Exercise06.java
@@ -0,0 +1,12 @@
+package Chap03.book.exercise;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int hight = 7;
+		double area = (lengthTop + lengthBottom) * hight / 2.0;
+		System.out.println(area);
+	}
+
+}
