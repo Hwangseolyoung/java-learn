56fba72936b7c3e33ec2d6f7bcc0d8c029cf241d
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-02-28 19:05:45 +0900

    if else, squirrelPlay

---
+++ src/chap04/lecture/ifelse/C03ElseIf.java
@@ -0,0 +1,23 @@
+package chap04.lecture.ifelse;
+
+public class C03ElseIf {
+	public static void main(String[] args) {
+		System.out.println("statement 1");
+		
+		if (false) {
+			System.out.println("statement 2");			
+		} else if (false) {
+			System.out.println("statement 3");			
+		} else {
+			System.out.println("statement 4");			
+		} 
+		// 마지막 else는 사용해도 되고 사용하지 않아도 된다.
+		
+		System.out.println("statement 5");
+		System.out.println("statement 6");
+		System.out.println("statement 7");
+		System.out.println("statement 8");
+		System.out.println("statement 9");
+	}
+
+}
