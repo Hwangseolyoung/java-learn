6f207345f9f346b4fe7daaeb7faf27c55fcb438a
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-10 16:59:15 +0900

    constructor

---
+++ src/chap06/book/example/s060702/CarExample.java
@@ -0,0 +1,9 @@
+package chap06.book.example.s060702;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+//		Car myCar = new Car(); 생성자를 작성했기때문에 기본생성자 작성하면 오류발생
+	}
+
+}
