2e40955bcdb891ea1264c0ad2347d0ecdd484a83
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-10 17:00:50 +0900

    field

---
+++ src/chap06/lecture/p01class/App01.java
@@ -0,0 +1,18 @@
+package chap06.lecture.p01class;
+
+public class App01 {
+	public static void main(String[] args) {
+		// 인스턴스를 사용하기 위해서 참조변수에 할당해 준다.
+		// 설계도 하나에 여러 인스턴스를 만들 수 있다. 
+		Car c1 = new Car(); 
+		Car c2 = new Car();
+		
+		// c1 객체 사용하는 코드...
+		
+		
+		
+		
+		
+	}
+
+}
