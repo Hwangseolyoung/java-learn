786488a6ba2dfcc4ea60dbf53f386440b9545e95
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-14 19:03:14 +0900

    access modifier

---
+++ src/chap06/lecture/p07package/App04.java
@@ -0,0 +1,14 @@
+package chap06.lecture.p07package;
+
+// 종종 잘못된 import를 사용할 수 있으니 주의하기.
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App04 {
+	public static void main(String[] args) {
+		Scanner scanner;
+		Arrays.copyOf(new int[] {1}, 0);
+		
+	}
+
+}
