4feadccc541c56de072577cd69748f0a3acbd9fc
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-15 10:38:24 +0900

    inheritance

---
+++ src/chap07/lecture/p01inheritance/Computer.java
@@ -0,0 +1,16 @@
+package chap07.lecture.p01inheritance;
+
+public class Computer {
+	// 필드
+	public String cpu;
+	public String ram;
+	public String ssd;
+	
+	// 생성자
+	
+	// 상속은 보통 메소드를 통해 일어난다.
+	// 메소드
+	public void executeGame() {
+		System.out.println("게임 실행");
+	}
+}
---
+++ src/chap07/lecture/p01inheritance/QuantumComputer.java
@@ -0,0 +1,23 @@
+package chap07.lecture.p01inheritance;
+
+public class QuantumComputer extends Computer {
+	
+	/*
+	// 필드
+	public String cpu;
+	public String ram;
+	public String ssd;
+
+	// 생성자
+
+	// 메소드
+	public void executeGame() {
+		System.out.println("게임을 실행합니다.");
+	}
+	*/
+	
+	public void executeQuantum() {
+		System.out.println("복잡한 연산을 합니다");
+	}
+
+}
