f64c5cee4025cd4cc2500797f5b6d98e03f1877f
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-15 16:24:08 +0900

    final

--- src/chap07/lecture/p04final/SubClass1.java
+++ src/chap07/lecture/p04final/SubClass1.java
@@ -1,5 +1,14 @@
 package chap07.lecture.p04final;
 
-public class SubClass1 {
+public class SubClass1 extends SuperClass1 {
+	
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+//	public void method2() {
+//		final 메소드는 재정의 안됨
+//	}
 
 }
