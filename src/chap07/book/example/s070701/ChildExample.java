b2ab799ece642839cceeab1f0ddfb0ba3def1fe9
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-16 11:53:39 +0900

    promotion

---
+++ src/chap07/book/example/s070701/ChildExample.java
@@ -0,0 +1,14 @@
+package chap07.book.example.s070701;
+
+public class ChildExample {
+	public static void main(String[] args) {
+		Child child = new Child();
+		
+		Parent parent = child;
+		parent.method1();
+		parent.method2();
+//		parent.method3();
+		child.method3();
+	}
+
+}
