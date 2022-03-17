54c20adf2b8a7a20419b16f50fe723daea490bfc
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-17 15:41:03 +0900

    default method

---
+++ src/chap08/lecture/p01interface/MyInterface1.java
@@ -0,0 +1,18 @@
+package chap08.lecture.p01interface;
+
+//interface 키워드 사용
+//명명관습: UpperCamelCase
+//개념일뿐이라서 인스턴스를 만들지 못함.
+//구성 요소는 모두 public
+public interface MyInterface1 {
+
+	// static final field
+	
+	// abstract method ************중요*******
+	
+	// default method
+	// 코드 블럭이 있는 인스턴스 메소드
+	
+	// static method 
+	
+}
