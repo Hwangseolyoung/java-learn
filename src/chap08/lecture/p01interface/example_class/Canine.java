748a427ec3346246b175d45ea968b6439461be90
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-17 15:40:02 +0900

    interface

---
+++ src/chap08/lecture/p01interface/example_class/Canine.java
@@ -0,0 +1,5 @@
+package chap08.lecture.p01interface.example_class;
+
+public abstract class Canine extends Animal{
+	// 재정의할 인터페이스가 없으므로 추상클래스로
+}
---
+++ src/chap08/lecture/p01interface/example_class/Feline.java
@@ -0,0 +1,6 @@
+package chap08.lecture.p01interface.example_class;
+
+public abstract class Feline extends Animal{
+	// 재정의할 인터페이스가 없으므로 추상클래스로
+
+}
