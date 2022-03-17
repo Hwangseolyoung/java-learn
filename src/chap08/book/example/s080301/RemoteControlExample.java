24dc2d917367a2bd4c9d5a1c4162c86cd6092410
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-17 16:19:20 +0900

    implements

---
+++ src/chap08/book/example/s080301/RemoteControlExample.java
@@ -0,0 +1,10 @@
+package chap08.book.example.s080301;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
