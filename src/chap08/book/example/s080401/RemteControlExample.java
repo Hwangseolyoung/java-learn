e8701d7dc9527709576a2eb7262e13d913af5405
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-17 16:44:27 +0900

    abstract interface

---
+++ src/chap08/book/example/s080401/RemteControlExample.java
@@ -0,0 +1,17 @@
+package chap08.book.example.s080401;
+
+public class RemteControlExample {
+	
+	RemoteControl rc = null;
+	
+	re = new Television();
+	rc.turnOn();
+	rc.turnOff();
+	
+	rc = new Audio();
+	rc.turnOn();
+	rc.turnOff();
+	
+	
+
+}
