924b2f6685a68c5edb384632009e15061cfbded8
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-17 15:41:35 +0900

    source

---
+++ src/chap08/lecture/p02polymorphism/App09.java
@@ -0,0 +1,14 @@
+package chap08.lecture.p02polymorphism;
+
+public class App09 {
+	public static void main(String[] args) {
+		Pet pet1 = new Retriever();
+		
+		
+		
+		// api source 보기
+		String str = "java";
+		
+	}
+
+}
