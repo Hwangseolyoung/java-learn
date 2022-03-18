b2f19e1df7ca2b9ad8381c2329ec6aea24cf757f
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-18 18:19:34 +0900

    static instance nested class

--- src/chap09/book/example/s090302/A.java
+++ src/chap09/book/example/s090302/A.java
@@ -1,5 +1,32 @@
 package chap09.book.example.s090302;
 
 public class A {
-
+	// instance
+	int field1;
+	void method1() {}
+	
+	// static
+	static int field2;
+	static void method2() {}
+	
+	class B{ // 모든 필드와 메소드에 접근 가능
+		void method() {
+			field1 = 10;
+			method1();
+			
+			field2 = 10;
+			method2();
+		}
+	}
+	
+	static class C { // 인스턴스 필드와 메소드에 접근 불가
+		void method() {
+//			field1 = 10;
+//			method1();
+			
+			field2 = 10;
+			method2();
+		}
+	}
+	
 }
