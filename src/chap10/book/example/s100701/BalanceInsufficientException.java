72e6e23ea787c2fdef3054494781ca7b7612f6f5
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-21 17:00:45 +0900

    application exception

---
+++ src/chap10/book/example/s100701/BalanceInsufficientException.java
@@ -0,0 +1,11 @@
+package chap10.book.example.s100701;
+
+public class BalanceInsufficientException extends Exception {
+	public BalanceInsufficientException() {
+		
+	}
+	
+	public BalanceInsufficientException(String message) {
+		super(message);
+	}
+}
