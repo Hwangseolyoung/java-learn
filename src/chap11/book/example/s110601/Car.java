6bc6bc2f780d2e4944c65083e2ddc971acac5c32
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-22 15:55:00 +0900

    getClass(), forName()

---
+++ src/chap11/book/example/s110601/Car.java
@@ -0,0 +1,29 @@
+package chap11.book.example.s110601;
+
+public class Car {
+	private String model;
+	private String owner;
+	
+	public Car() {
+		
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	
+
+}
