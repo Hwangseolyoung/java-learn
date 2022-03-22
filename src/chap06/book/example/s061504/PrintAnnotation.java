2ead83c82cb58c2967e0a26586e2eacd3b1b4c0a
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-22 17:07:01 +0900

    annotation

---
+++ src/chap06/book/example/s061504/PrintAnnotation.java
@@ -0,0 +1,18 @@
+package chap06.book.example.s061504;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface PrintAnnotation {
+	String value() default "-";
+	int number() default 15;
+	
+
+}
