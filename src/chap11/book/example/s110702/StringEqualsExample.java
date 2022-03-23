0ed51fbc9275ff3cf8826c700c2f71a003719680
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-23 16:45:46 +0900

    equals

---
+++ src/chap11/book/example/s110702/StringEqualsExample.java
@@ -0,0 +1,21 @@
+package chap11.book.example.s110702;
+
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVar1 = new String("신만철");
+		String strVar2 = "신만철";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("같은 String 객체를 참조");
+		} else {
+			System.out.println("다른 String 객체를 참조");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("같은 String 객체를 참조");
+		} else {
+			System.out.println("다른 String 객체를 참조");
+		}
+	}
+
+}
