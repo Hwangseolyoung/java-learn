4d827f62b303ed8f938daaeea80bcea89a00b9ee
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-23 10:54:03 +0900

    exercise11

---
+++ src/chap11/book/exercise/p11/IntegerCompareExample.java
@@ -0,0 +1,19 @@
+package chap11.book.exercise.p11;
+
+public class IntegerCompareExample {
+	public static void main(String[] args) {
+		Integer obj1 = 100;
+		Integer obj2 = 100;
+		Integer obj3 = 300;
+		Integer obj4 = 300;
+		
+		System.out.println(obj1 == obj2); // true
+		System.out.println(obj3 == obj4); // false
+		 
+		System.out.println(obj1.equals(obj2)); // true
+		System.out.println(obj3.equals(obj4)); // true
+	
+		
+	}
+
+}
