2ebb5c8236cbc59c86c2b2e16aaaca510e74249a
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-23 10:53:19 +0900

    auto boxin, unboxing

---
+++ src/chap11/lecture/p03wrapper/App01.java
@@ -0,0 +1,19 @@
+package chap11.lecture.p03wrapper;
+
+public class App01 {
+	public static void main(String[] args) {
+		// java data type
+		// primitive type (8개) : byte, short, int, long, char, float, double, boolean
+		// reference type (무한) :  나머지 모두
+		
+		Object[] arr = new Object[3];
+		arr[0] = "java";
+		arr[1] = 3; // 원칙상으로는 안되나 예외적 문법으로 가능함
+		arr[2] = new Integer(3); //  java 4 
+		
+		// wrapper class (8개)
+		// Byte, Short, Integer, Long, Character, Float, Double, Boolean
+		
+	}
+
+}
