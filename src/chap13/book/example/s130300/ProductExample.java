f427311b33bceaf09b50cb7deebfdd443e03f83e
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-24 15:55:04 +0900

    multi type parameter

---
+++ src/chap13/book/example/s130300/ProductExample.java
@@ -0,0 +1,11 @@
+package chap13.book.example.s130300;
+
+public class ProductExample {
+	public static void main(String[] args) {
+		Product<Tv, String> product1 = new Product<>();
+		product1.setKind(new Tv());
+		
+		
+ 	}
+
+}
---
+++ src/chap13/book/example/s130300/Tv.java
@@ -0,0 +1,8 @@
+package chap13.book.example.s130300;
+
+public class Tv {
+	public static void main(String[] args) {
+		
+	}
+
+}
