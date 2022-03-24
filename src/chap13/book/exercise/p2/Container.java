aebb4ffc818589714dd5e2e9cd3db0dc334a313e
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-24 15:55:49 +0900

    exercise2

---
+++ src/chap13/book/exercise/p2/Container.java
@@ -0,0 +1,13 @@
+package chap13.book.exercise.p2;
+
+public class Container<T> {
+	private T container;
+
+	public T getContainer() {
+		return container;
+	}
+
+	public void setContainer(T container) {
+		this.container = container;
+	}
+}
