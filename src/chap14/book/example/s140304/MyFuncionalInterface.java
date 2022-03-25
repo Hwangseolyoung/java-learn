42481f4193991945638c38e5180185052121cbb6
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-25 11:49:30 +0900

    lambda expression

---
+++ src/chap14/book/example/s140304/MyFuncionalInterface.java
@@ -0,0 +1,6 @@
+package chap14.book.example.s140304;
+
+@FunctionalInterface
+public interface MyFuncionalInterface {
+	public int method(int x, int y);
+}
---
+++ src/chap14/book/example/s140304/MyFuncionalInterfaceExample.java
@@ -0,0 +1,32 @@
+package chap14.book.example.s140304;
+
+public class MyFuncionalInterfaceExample {
+	public static void main(String[] args) {
+		MyFuncionalInterface fi;
+		
+		fi = (x, y) -> {
+			int result = x + y;
+			return result;
+		};
+		System.out.println(fi.method(2, 5));
+		
+		fi = (x, y) -> {
+			return x + y;
+		};
+		System.out.println(fi.method(2, 5));
+		
+		fi = (x, y) -> x + y;
+		System.out.println(fi.method(2, 5));
+		
+		fi = (x, y) -> sum(x, y);
+		System.out.println(fi.method(2, 5));
+		
+		
+		
+	}
+	
+	public static int sum(int x, int y) {
+		return (x + y);
+	}
+
+}
