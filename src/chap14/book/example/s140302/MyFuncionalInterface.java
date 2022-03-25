168bf15f537117df009b5c279e409e81d02b7a69
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-03-25 11:48:34 +0900

    lambda expression

---
+++ src/chap14/book/example/s140302/MyFuncionalInterface.java
@@ -0,0 +1,6 @@
+package chap14.book.example.s140302;
+
+@FunctionalInterface
+public interface MyFuncionalInterface {
+	public void method();
+}
---
+++ src/chap14/book/example/s140303/MyFuncionalInterface.java
@@ -0,0 +1,6 @@
+package chap14.book.example.s140303;
+
+@FunctionalInterface
+public interface MyFuncionalInterface {
+	public void method(int x);
+}
