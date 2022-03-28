ebae5be9d1f19ff7b0ee06b6f9e982c149e60f46
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-28 21:08:00 +0900

    remove

---
+++ src/chap15/lecture/p01list/App09.java
@@ -0,0 +1,21 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App09 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < 9; i++) {
+			list.add(i);
+		}
+		System.out.println(list);
+		
+		// 홀수 element 삭제
+		list.removeIf(a -> a % 2 == 1);
+		System.out.println(list);
+		
+		
+	}
+
+}
