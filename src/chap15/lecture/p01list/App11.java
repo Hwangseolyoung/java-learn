7f00df7d007e10f4653902d4d2f3b79f90ec4aa7
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-03-29 19:56:44 +0900

    list

---
+++ src/chap15/lecture/p01list/App11.java
@@ -0,0 +1,32 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App11 {
+	public static void main(String[] args) {
+		List<List<Integer>> list = new ArrayList<>();
+		list.add(Arrays.asList(95, 86));
+		list.add(Arrays.asList(83, 92, 96));
+		list.add(Arrays.asList(78, 83, 93, 87, 88));
+		
+		// 합계(881), 평균(88.1) 구하기
+		int sum = 0, cnt = 0;
+		double avg = 0.0;
+		for(List<Integer> row : list) {
+//			System.out.println(row);
+			for(Integer score : row) {
+//				System.out.println(score);
+				sum += score; // 합계
+				cnt++; // 갯수
+			}
+		}
+		
+		System.out.println(sum);
+		System.out.println(sum / (double) cnt);
+		 
+		
+	}
+
+}
