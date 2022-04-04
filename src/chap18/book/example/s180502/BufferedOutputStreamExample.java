9bb28f9983a30e024ff12c1d1f37a7df564cf388
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-04-04 12:58:12 +0900

    BufferedOutputStream

--- src/chap18/book/example/s180502/BufferedOutputStreamExample.java
+++ src/chap18/book/example/s180502/BufferedOutputStreamExample.java
@@ -5,7 +5,7 @@ import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class BufferOutputStreamExample {
+public class BufferedOutputStreamExample {
 	public static void main(String[] args) throws Exception {
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
